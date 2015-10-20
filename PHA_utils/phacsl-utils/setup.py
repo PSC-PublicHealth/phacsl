ec55e9f0a8ee6141db224ea4ad4e19beb227f4b7
Jay DePasse <depasse@macula.psc.edu>
2015-10-20 10:41:48 -0400

    refactoring to fit in phacsl namespace
    - renamed package to phacsl-utils
    - moved tests under utils

--- PHA_utils/phacsl-utils/setup.py
+++ PHA_utils/phacsl-utils/setup.py
@@ -2,17 +2,17 @@
 
 from distutils.core import setup
 
-setup(name='PHA_utils',
+setup(name='phacsl-utils',
       version='0.0.1',
       description='Generic utilities shared by PHA software',
       author='Joel Welling',
       author_email='welling@psc.edu',
       #url='https://www.python.org/sigs/distutils-sig/',
-      packages=['PHA_utils', 
-                'PHA_utils.misc',
-                'PHA_utils.formats',
-                'PHA_utils.notes',
-                'tests'
+      packages=['phacsl.utils', 
+                'phacsl.utils.misc',
+                'phacsl.utils.formats',
+                'phacsl.utils.notes',
+                'phacsl.utils.tests'
                 ],
       package_dir = {'': 'src'}
      )
